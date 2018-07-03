209455b70412d986d03525a01522cd28b60b24d0
vendi <svitlana.vakulenko@gmail.com>
2018-07-03 23:51:47 +0200

    fix

--- nmt/conversational_data/generated_data/data_generator.py
+++ nmt/conversational_data/generated_data/data_generator.py
@@ -9,25 +9,25 @@ import random
 # load patterns
 with open('search_patterns.txt') as f:
     patterns = f.readlines()
-# load search terms
-with open('search_terms.txt') as f:
-    terms = f.readlines()
 
 random.shuffle(patterns)
 
 for pattern in patterns:
-    # shuffle terms
-    random.shuffle(terms)
+    # load search terms
+    with open('search_terms.txt') as f:
+        terms = f.readlines()
+        # shuffle terms
+        random.shuffle(terms)
     # generate samples
     user_samples = []
     agent_samples = []
-    for i in xrange(len(terms)):
+    while terms:
         sample = ''
         search_terms = []
         for c in pattern.strip():
             if c == '*':
                 # draw a random search term
-                term = terms[i].strip()
+                term = terms.pop().strip()
                 search_terms.append(term)
                 sample += term
             else:
@@ -35,16 +35,15 @@ for pattern in patterns:
         print sample
         user_samples.append(sample)
         agent_samples.append('search ' + ' '.join(search_terms))
-    assert len(user_samples) == len(terms) == len(agent_samples)
     
     with open('train.agent', 'a') as f_agent, open('train.user', 'a') as f_user:
-        f_user.writelines('\n'.join(user_samples[:40]) + '\n')
-        f_agent.writelines('\n'.join(agent_samples[:40]) + '\n')
+        f_user.writelines('\n'.join(user_samples[:30]) + '\n')
+        f_agent.writelines('\n'.join(agent_samples[:30]) + '\n')
 
     with open('dev.agent', 'a') as f_agent, open('dev.user', 'a') as f_user:
-        f_user.writelines('\n'.join(user_samples[41:51]) + '\n')
-        f_agent.writelines('\n'.join(agent_samples[41:51]) + '\n')
+        f_user.writelines('\n'.join(user_samples[31:41]) + '\n')
+        f_agent.writelines('\n'.join(agent_samples[31:41]) + '\n')
 
     with open('test.agent', 'a') as f_agent, open('test.user', 'a') as f_user:
-        f_user.writelines('\n'.join(user_samples[52:]) + '\n')
-        f_agent.writelines('\n'.join(agent_samples[52:]) + '\n')
+        f_user.writelines('\n'.join(user_samples[42:]) + '\n')
+        f_agent.writelines('\n'.join(agent_samples[42:]) + '\n')
